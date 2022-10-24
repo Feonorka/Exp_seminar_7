b4a3dd82ce179367882c6497ce630d7743b55fea
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-10-24 22:46:07 +0300

    Решены 53 и 55 задачи

--- Lvl_53/Program.cs
+++ Lvl_53/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)        // 0 - rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)    // 1- columns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} | ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void ArraySwitch(int[,] matrix)
+{
+    int[,] result = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    int temp = default;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+
+}
+int[,] array2D = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2D);
+ArraySwitch(array2D);
+PrintMatrix(array2D);
--- Lvl_55/Program.cs
+++ Lvl_55/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)        // 0 - rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)    // 1- columns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} | ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] TreanspMatrix(int[,] matrix)
+{
+    int[,] res = new int[matrix.GetLength(1), matrix.GetLength(0)];
+    
+
+    for (int i = 0; i < matrix.GetLength(0); i++)        // 0 - rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)    // 1- columns
+        {
+            res[j, i] = matrix[i, j];
+        }
+    }
+
+    return res;
+}
+
+int[,] array2D = CreateMatrixRndInt(4, 4, -100, 100);
+PrintMatrix(array2D);
+Console.WriteLine();
+if (array2D.GetLength(1) == array2D.GetLength(0))
+{
+    int[,]result = TreanspMatrix(array2D);
+    PrintMatrix(result);
+}
+else Console.Write("Ваша матрица не квадратная");
